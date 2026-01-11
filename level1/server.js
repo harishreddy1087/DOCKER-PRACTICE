a08a99fe762dc1d8acba7479c4b282029cf4a1ac
harishreddy1087 <harishreddy1087@gmail.com>
2026-01-11 16:17:26 +0530

    docker file practice level1

--- level1/server.js
+++ level1/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from inside the Docker Container!');
+  res.end('Hello reddy from inside the Docker Container!');
 });
 
 server.listen(3000, () => {
