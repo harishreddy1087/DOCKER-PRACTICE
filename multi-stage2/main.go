591f57cb88caf0d38da6f3a8ebac5550a246ceee
harishreddy1087 <harishreddy1087@gmail.com>
2026-01-12 01:37:53 +0530

    multi-stage2

--- multi-stage2/main.go
+++ multi-stage2/main.go
@@ -1,7 +1,7 @@
 package main
-
 import "fmt"
 
 func main() {
-    fmt.Println("Hello! This is a secure, multi-stage Go binary running on Alpine!")
+    fmt.Println("Hello from a tiny Go container!")
+}
 }
